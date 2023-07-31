dc4f6bb79a124b41bb64e0e2f9aab7f02a4c7d2b
Meet Patel <meetshooter123@gmail.com>
2023-07-31 11:11:04 -0600

    added event handler

--- SignupHandler.js
+++ SignupHandler.js
@@ -1,3 +1,15 @@
 email = document.getElementById("Email")
 
 email.addEventListener("blur", emailValidator);
+
+function emailValidator (event){
+    email = event.target.value
+    
+    if (/^([A-Za-z]|[0-9])+$/.test(email)){
+        console.log("Valid Email")
+    }
+
+    else{
+        console.log("Invalid Email")
+    }
+}
